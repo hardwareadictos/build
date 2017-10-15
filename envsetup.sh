1bba227e58434b143f9f43c89cdf36d840e78454
Martinusbe <martinusbe@gmail.com>
2017-10-15 23:59:18 +0200

    envsetup: correct path to repopick in vendor
    Change-Id: I2636d8028ffb0c2680dbfa9ec8f8bc80e13a5945

--- envsetup.sh
+++ envsetup.sh
@@ -1638,7 +1638,7 @@ function reposync() {
 
 function repopick() {
     T=$(gettop)
-    $T/build/tools/repopick.py $@
+    $T/vendor/validus/build/tools/repopick.py $@
 }
 
 function fixup_common_out_dir() {
